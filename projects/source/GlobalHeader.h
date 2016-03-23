7e8fa3587e670451c46f96e20163a8d00d651b08
ft-lab <y_yutaka@fa2.so-net.ne.jp>
2016-03-23 23:27:57 +0900

    upload windows projects (VS2013/VS2015)

---
+++ projects/source/GlobalHeader.h
@@ -0,0 +1,17 @@
+﻿/**
+ *  @file   GlobalHeader.h
+ *  @brief  共通して使用する変数など.
+ */
+
+#ifndef _GLOBALHEADER_H
+#define _GLOBALHEADER_H
+
+#include "sxsdk.cxx"
+
+/**
+ * プラグインインターフェイス派生クラスのプラグインID.
+ */
+#define MY_PLUGIN_ID sx::uuid_class("22709F02-8B60-4221-BF7E-1E908BC84C7C")
+
+
+#endif
